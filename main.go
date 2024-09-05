aaaa0231204f8d100ff512619b5872f94eeb44a9
Syanshine <shpli943@gmail.com>
2024-09-05 23:59:30 +0700

    Add files via upload

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nama string = " Nama : Syana"
+	var umur int = 17
+	var alamat string = " Alamat : Depok"
+	var berkacamata bool = true
+	var minuskiri float64 = 0.50
+	var minuskanan float64 = 0.75
+	var hasilminus float64
+	hasilminus = minuskiri + minuskanan
+
+	fmt.Println(nama)
+	fmt.Println(umur)
+	fmt.Println(alamat)
+	fmt.Println(berkacamata)
+	fmt.Println(minuskiri)
+	fmt.Println(minuskanan)
+	fmt.Println(hasilminus)
+	
+}
